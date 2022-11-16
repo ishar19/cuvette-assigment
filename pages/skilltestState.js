bbb4c3f0a7dd9839f8a555756c97c454a98595e1
ishar <isharjain19@gmail.com>
2022-11-16 12:37:26 +0530

    build error fixed

--- pages/skilltestState.js
+++ pages/skilltestState.js
@@ -20,3 +20,7 @@ export const answerState = atom({
     key: 'answerState', // unique ID (with respect to other atoms/selectors)
     default: 7, // default value (aka initial value)
 });
+
+export default function skillState(){
+    return (<></>)
+}
